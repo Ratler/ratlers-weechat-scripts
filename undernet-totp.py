390a64c3a727d6680e1a8ff3f33ea22dfa362d2c
Stefan Wold <ratler@stderr.eu>
2013-12-17 20:07:05 +0100

    Added signal handling and detection of successful auth
    undernet_seed can now be either base32 string or a 20 byte HEX string.

--- undernet-totp.py
+++ undernet-totp.py
@@ -24,8 +24,14 @@ SCRIPT_NAME    = "undernet-totp"
 SCRIPT_AUTHOR  = "Stefan Wold <ratler@stderr.eu>"
 SCRIPT_VERSION = "0.1dev"
 SCRIPT_LICENSE = "GPL3"
-SCRIPT_DESC    = "UnderNET X service OATH-TOTP authentication"
+SCRIPT_DESC    = "UnderNET X OTP authentication"
 SCRIPT_COMMAND = "uotp"
+
+HOOKS = {}
+
+SETTINGS = {
+    "otp_server_name": "off",
+}
 
 import_ok = True
 
@@ -37,43 +43,94 @@ except ImportError:
 
 try:
     import hmac
-    import base64
-    import hashlib
-    import struct
-    import time
     import re
+    from base64 import b32decode
+    from hashlib import sha1
+    from struct import pack, unpack
+    from time import time
+    from binascii import unhexlify
 except ImportError as err:
     print "Missing module(s) for %s: %s" % (SCRIPT_NAME, err)
     import_ok = False
 
 
+def unhook(hook):
+    global HOOKS
+
+    if hook in HOOKS:
+        weechat.unhook(HOOKS[hook])
+        del HOOKS[hook]
+
+
+def unhook_all():
+    for hook in ['notice', 'modifier']:
+        unhook(hook)
+
+
+def hook_all(server):
+    global HOOKS
+
+    HOOKS['notice']   = weechat.hook_signal("%s,irc_raw_in_notice" % server, "auth_success_cb", "")
+    HOOKS['modifier'] = weechat.hook_modifier("irc_out_privmsg", "totp_login_modifier_cb", "")
+
+
 def totp_login_modifier_cb(data, modifier, modifier_data, cmd):
-    if re.match(r'^PRIVMSG x@channels.undernet.org :login .+ .+', cmd):
+    if re.match(r'(?i)^PRIVMSG x@channels.undernet.org :login .+ .+', cmd):
         otp = generate_totp()
         if otp is not None:
             cmd += " %s" % otp
     return cmd
 
 
-def get_otp_cb(data, buffer, args):
-    otp = generate_totp()
-    if otp is not None:
-        weechat.prnt("", "UnderNET OTP: %s" % otp)
+def auth_success_cb(data, signal, signal_data):
+    if signal_data.startswith(":X!cservice@undernet.org NOTICE"):
+        if re.match(r'^:X!cservice@undernet.org NOTICE .+ :AUTHENTICATION SUCCESSFUL', signal_data):
+            unhook_all()
+
     return weechat.WEECHAT_RC_OK
 
 
-def generate_totp():
-    seed = weechat.string_eval_expression("${sec.data.undernet_token}", {}, {}, {})
-    if seed is "":
-        weechat.prnt("", "No Oath-TOTP base32 encoded secret set, use: /secure set undernet_token <secret>")
-        return None
-    s = base64.b32decode(seed, True)
-    m = struct.pack(">Q", int(time.time())//30)
-    h = hmac.new(s, m, hashlib.sha1).digest()
-    o = ord(h[19]) & 15
-    h = (struct.unpack(">I", h[o:o+4])[0] & 0x7fffffff) % 1000000
+def signal_cb(data, signal, signal_data):
+    value = weechat.config_get_plugin('otp_server_name')
 
-    return '%06d' % h
+    if signal == 'irc_server_connecting':
+        if value is not "off" and value == signal_data:
+            hook_all(signal_data)
+    elif signal == 'irc_server_disconnected':
+        if value is not "off" and value == signal_data:
+            unhook_all()
+
+    return weechat.WEECHAT_RC_OK
+
+
+def get_otp_cb(data, buffer, args):
+    otp = generate_totp()
+
+    if otp is not None:
+        weechat.prnt("", "UnderNET OTP: %s" % otp)
+
+    return weechat.WEECHAT_RC_OK
+
+
+def generate_totp(period=30):
+    seed = weechat.string_eval_expression("${sec.data.undernet_seed}", {}, {}, {})
+
+    if seed is "":
+        weechat.prnt("", "No OATH-TOTP secret set, use: /secure set undernet_seed <secret>")
+        return None
+
+    if len(seed) == 40:  # Assume hex format
+        seed = unhexlify(seed)
+    else:
+        seed = b32decode(seed, True)
+
+    t = pack(">Q", int(time() / period))
+    _hmac = hmac.new(seed, t, sha1).digest()
+    o = ord(_hmac[19]) & 15
+    otp = (unpack(">I", _hmac[o:o+4])[0] & 0x7fffffff) % 1000000
+
+    return '%06d' % otp
+
 
 if __name__ == "__main__" and import_ok:
     if weechat.register(SCRIPT_NAME, SCRIPT_AUTHOR, SCRIPT_VERSION, SCRIPT_LICENSE,
@@ -83,5 +140,10 @@ if __name__ == "__main__" and import_ok:
             weechat.prnt("", "%s requires WeeChat >= 0.4.2 for secure_data support." % SCRIPT_NAME)
             weechat.command("", "/wait 1ms /python unload %s" % SCRIPT_NAME)
 
-        weechat.hook_command(SCRIPT_COMMAND, "UnderNET X OATH-TOTP", "", "", "", "get_otp_cb", "")
-        weechat.hook_modifier("irc_out_privmsg", "totp_login_modifier_cb", "")
+        weechat.hook_command(SCRIPT_COMMAND, "UnderNET X OTP", "", "", "", "get_otp_cb", "")
+        weechat.hook_signal("irc_server_connecting", "signal_cb", "")
+        weechat.hook_signal("irc_server_disconnected", "signal_cb", "")
+
+        for option, default_value in SETTINGS.items():
+            if weechat.config_get_plugin(option) == "":
+                weechat.config_set_plugin(option, default_value)
