f582d691febdfee3bedbea710220352087994880
Stefan Wold <ratler@stderr.eu>
2014-01-25 21:59:27 +0100

    responsive_layout: Fixed help texts and added more /rlayout <options>
    It's now possible to remove and list rlayout configuration.

--- responsive_layout.py
+++ responsive_layout.py
@@ -23,16 +23,16 @@
 # Source and changes available on GitHUB: https://github.com/Ratler/ratlers-weechat-scripts
 #
 # Configuration:
-#
+#  /set plugins.var.python.responsive_layout.nicklist <on|off>  -  Enable or disable global nicklist for layouts
 # Commands:
-#
+#  /rlayout
 
 
 SCRIPT_NAME    = "responsive_layout"
 SCRIPT_AUTHOR  = "Stefan Wold <ratler@stderr.eu>"
 SCRIPT_VERSION = "0.1dev"
 SCRIPT_LICENSE = "GPL3"
-SCRIPT_DESC    = "Responsive layout"
+SCRIPT_DESC    = "Responsive layout automatically apply layouts based on the terminals current dimensions."
 SCRIPT_COMMAND = "rlayout"
 
 SETTINGS = {
@@ -57,6 +57,10 @@ except ImportError as err:
     import_ok = False
 
 
+def _print(message, buf=""):
+    weechat.prnt(buf, "%s: %s" % (SCRIPT_NAME, message))
+
+
 def responsive_cb(data, signal, signal_data):
     term_height = int(weechat.info_get("term_height", ""))
     term_width = int(weechat.info_get("term_width", ""))
@@ -73,12 +77,12 @@ def responsive_cb(data, signal, signal_data):
             apply_layout = LAYOUT_LIST[-1][0]
 
         if layout_exist(apply_layout) and not layout_current(apply_layout):
-            weechat.prnt("", "Applying layout %s" % apply_layout)
+            _print("Applying layout %s" % apply_layout)
             weechat.command("", "/layout apply %s" % apply_layout)
             toggle_nick_list(apply_layout)
 
     except ValueError:
-        weechat.prnt("", "height or width is not a number, ignored.")
+        _print("Height or width is not in number form, ignoring.")
 
     return weechat.WEECHAT_RC_OK
 
@@ -124,31 +128,38 @@ def toggle_nick_list(layout):
         weechat.command("", "/bar hide nicklist")
 
 
+def rlayouts_list():
+    """
+    Return a list of configured rlayouts.
+    """
+    layouts = []
+    pattern = re.compile(r"^plugins\.var\.python\.%s\.layout\.(.+)\." % SCRIPT_NAME)
+    infolist = weechat.infolist_get("option", "", "plugins.var.python.%s.layout.*" % SCRIPT_NAME)
+
+    while weechat.infolist_next(infolist):
+        layout = re.search(pattern, weechat.infolist_string(infolist, "full_name")).groups()
+        if layout[0] not in layouts:
+            layouts.append(layout[0])
+
+    weechat.infolist_free(infolist)
+
+    return layouts
+
+
 def update_layout_list():
     """
     Updates global LAYOUT_LIST with a sorted array containing layout tuples, ie (layout_name, width, height)
     """
     global LAYOUT_LIST
 
-    layouts = []
     layout_tuples = []
-    pattern = re.compile(r"^plugins\.var\.python\.%s\.layout\.(.+)\." % SCRIPT_NAME)
-    infolist = weechat.infolist_get("option", "", "plugins.var.python.%s.layout.*" % SCRIPT_NAME)
 
-    while weechat.infolist_next(infolist):
-        layout = re.search(pattern, weechat.infolist_string(infolist, "full_name")).groups()
-        if layout not in layouts:
-            layouts.append(layout)
-
-    weechat.infolist_free(infolist)
-
-    for layout in layouts:
+    for layout in rlayouts_list():
         width = weechat.config_get_plugin("layout.%s.width" % layout)
         height = weechat.config_get_plugin("layout.%s.height" % layout)
 
         if width is not "" and height is not "":
-            tup = ('%s' % layout, int(width), int(height))
-            layout_tuples.append(tup)
+            layout_tuples.append((layout, int(width), int(height)))
 
     layout_tuples.sort(key=itemgetter(1, 2))
     LAYOUT_LIST = layout_tuples
@@ -166,8 +177,8 @@ def rlayout_cmd_cb(data, buffer, args):
     if len(argv) == 0:
         return weechat.WEECHAT_RC_OK
 
-    if len(argv) < 2:
-        weechat.prnt("", "Too few args")
+    if argv[0] != "list" and len(argv) < 2:
+        _print("Too few arguments for option '%s'." % argv[0])
         return weechat.WEECHAT_RC_OK
 
     if argv[0] == "size":
@@ -179,9 +190,9 @@ def rlayout_cmd_cb(data, buffer, args):
                 weechat.config_set_plugin("layout.%s.height" % layout, height)
                 update_layout_list()
             else:
-                weechat.prnt("", "Layout '%s' doesn't exist, see /help layout to create one.")
+                _print("Layout '%s' doesn't exist, see /help layout to create one." % layout)
         except ValueError:
-            weechat.prnt("", "Invalid number of arguments, ex /rlayout size <layout> <width> <height>")
+            _print("Too few arguments for option '%s'" % argv[0])
     elif argv[0] == "nicklist":
         try:
             layout, nicklist = argv[1].split(" ")
@@ -190,11 +201,30 @@ def rlayout_cmd_cb(data, buffer, args):
                 if nicklist == "on" or nicklist == "off":
                     weechat.config_set_plugin("layout.%s.nicklist" % layout, nicklist)
                 else:
-                    weechat.prnt("", "Invalid argument '%s', ex /rlayout nicklist <layout> <on|off>" % nicklist)
+                    _print("Invalid argument '%s' for option '%s'." % (nicklist, argv[0]))
             else:
-                weechat.prnt("", "Layout '%s' doesn't exist, see /help layout to create one.")
+                _print("Layout '%s' doesn't exist, see /help layout to create one." % layout)
         except ValueError:
-            weechat.prnt("", "Invalid number of arguments, ex /rlayout nicklist <layout> <on|off>")
+            _print("Too few arguments for option '%s'" % argv[0])
+    elif argv[0] == "remove":
+        if argv[1] in rlayouts_list():
+            for option in ["width", "height", "nicklist"]:
+                weechat.config_unset_plugin("layout.%s.%s" % (argv[1], option))
+            _print("Removed rlayout '%s'" % argv[1])
+        else:
+            _print("Could not remove '%s', rlayout not found." % argv[1])
+    elif argv[0] == "list":
+        if len(rlayouts_list()) == 0:
+            _print("No configuration set.")
+        else:
+            for rlayout in rlayouts_list():
+                width = weechat.config_get_plugin("layout.%s.width" % rlayout)
+                height = weechat.config_get_plugin("layout.%s.height" % rlayout)
+                nicklist = weechat.config_get_plugin("layout.%s.nicklist" % rlayout)
+                msg = "[%s] width: %s, height: %s" % (rlayout, width, height)
+                if nicklist is not "":
+                    msg += ", nicklist: %s" % nicklist
+                _print(msg)
 
     return weechat.WEECHAT_RC_OK
 
@@ -205,20 +235,30 @@ def rlayout_completion_bool_cb(data, completion_item, buffer, completion):
     return weechat.WEECHAT_RC_OK
 
 
+def rlayout_completion_layout_list_cb(data, completion_item, buffer, completion):
+    for rlayout in rlayouts_list():
+        weechat.hook_completion_list_add(completion, rlayout, 0, weechat.WEECHAT_LIST_POS_SORT)
+    return weechat.WEECHAT_RC_OK
+
+
 if __name__ == "__main__" and import_ok:
     if weechat.register(SCRIPT_NAME, SCRIPT_AUTHOR, SCRIPT_VERSION, SCRIPT_LICENSE, SCRIPT_DESC, "", ""):
         version = weechat.info_get("version_number", "") or 0
         if int(version) < 0x00040300:
-            weechat.prnt("", "%s requires WeeChat >= 0.4.3 for terminal height and width support." % SCRIPT_NAME)
+            _print("Requires WeeChat >= 0.4.3 for terminal height and width support." % SCRIPT_NAME)
             weechat.command("", "/wait 1ms /python unload %s" % SCRIPT_NAME)
 
         weechat.hook_command(SCRIPT_COMMAND,
                              "WeeChat responsive layout",
-                             "size <layout> <width> <height> || nicklist <layout> <on|off>",
-                             "    size: bla bla\n"
-                             "nicklist: blabla\n\n",
+                             "size <layout> <width> <height> || nicklist <layout> <on|off> || remove <layout> || list",
+                             "    size: set max size (width and height) for layout to be automatically applied\n"
+                             "nicklist: show or hide nicklist bar when layout is automatically applied\n"
+                             "  remove: remove settings for responsive layout\n"
+                             "    list: list current configuration\n\n",
                              "size %(layouts_names)"
-                             " || nicklist %(layouts_names) %(rlayout_bool_value)",
+                             " || nicklist %(layouts_names) %(rlayout_bool_value)"
+                             " || remove %(rlayouts_names)"
+                             " || list",
                              "rlayout_cmd_cb",
                              "")
 
@@ -229,5 +269,6 @@ if __name__ == "__main__" and import_ok:
             weechat.config_set_desc_plugin(option, '%s (default: %s)' % (default_value[1], default_value[0]))
 
         weechat.hook_completion("rlayout_bool_value", "list of bool values", "rlayout_completion_bool_cb", "")
+        weechat.hook_completion("rlayouts_names", "list of rlayouts", "rlayout_completion_layout_list_cb", "")
         update_layout_list()
         hook = weechat.hook_signal("signal_sigwinch", "responsive_cb", "")
